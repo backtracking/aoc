79eacf6929cd2c42bd1b0dceb52f2570b49f3c62
Jean-Christophe Filliatre <Jean-Christophe.Filliatre@cnrs.fr>
2024-12-08 19:56:40 +0100

    day 7

---
+++ 24/day7.ml
@@ -0,0 +1,20 @@
+
+open Format
+open Lib
+
+let sum = ref 0
+
+let rec solvable t = function
+  | [] -> assert false
+  | [x] -> x = t
+  | x :: y :: xl -> solvable t ((x + y) :: xl) ||
+                    solvable t ((x * y) :: xl)
+
+let f s =
+  let t, xl = split2 ~sep:':' s in
+  let t = int_of_string t and xl = split_ints xl in
+  if solvable t xl then sum += t
+
+let () = iter_lines stdin f
+let () = printf "%d@." !sum
+
