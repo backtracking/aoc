724b3a5fc38eeccdf7668a0bba342708a8ae5e07
Jean-Christophe Filliatre <Jean-Christophe.Filliatre@cnrs.fr>
2024-12-10 09:44:42 +0100

    day 10, better

--- 24/day10.ml
+++ 24/day10.ml
@@ -5,17 +5,14 @@ module H = Hashtbl
 
 let g = map (fun _ -> Lib.int_of_char) (read stdin)
 
-let nines = ref []
-let () = iter (fun p c -> if c = 9 then nines := p :: !nines) g
+module S = Set.Make(struct type t = position let compare = compare end)
 
-let path = Lib.memo (fun path (p, q) ->
-  p = q ||
+let count = Lib.memo (fun count p ->
   let x = get g p in
-  fold4 (fun p' n b -> n = x+1 && path (p', q) || b) g p false
+  if x = 9 then S.singleton p else
+  fold4 (fun p' n s -> if n = x+1 then S.union s (count p') else s) g p S.empty
 )
 
-let add p s q = if path (p, q) then s+1 else s
-let start p = List.fold_left (add p) 0 !nines
-let start p c s = if c = 0 then s + start p else s
+let start p c s = if c = 0 then s + S.cardinal (count p) else s
 
 let () = printf "%d@." (fold start g 0)
